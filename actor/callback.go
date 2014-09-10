dc997e35a56fca2a4c334d16b315583520ab4e5f
funkygao <funky.gao@gmail.com>
2014-09-10 17:49:54 +0800

    resolve problem of 'too many open files'

--- actor/callback.go
+++ actor/callback.go
@@ -12,12 +12,13 @@ func (this *Actor) callback(m march) {
 	buf, _ := json.Marshal(m)
 
 	body := bytes.NewBuffer(buf)
-	url := fmt.Sprintf(this.server.String("callback_url", ""), m.Evt)
+	url := fmt.Sprintf(this.server.String("callback_url", ""), m.Evt, string(buf))
 	log.Debug("%+v %s %s", m, string(buf), url)
 
 	res, err := http.Post(url, "application/json", body)
+	defer res.Body.Close()
 	if err != nil {
-		log.Error(err)
+		log.Error("post error: %s", err.Error())
 	} else {
 		if res.StatusCode != http.StatusOK {
 			log.Error("callback error: %+v", res)
