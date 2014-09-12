053aa3d11039f3f6f9b83f726b69330e83e6d079
funkygao <funky.gao@gmail.com>
2014-09-12 18:31:16 +0800

    benchmark job scheduler

--- actor/jobs_test.go
+++ actor/jobs_test.go
@@ -2,7 +2,9 @@ package actor
 
 import (
 	"github.com/funkygao/assert"
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestMarchesChunks(t *testing.T) {
@@ -22,5 +24,46 @@ func TestMarchesChunks(t *testing.T) {
 		}
 	}
 	assert.Equal(t, 3, maxChunkLen)
+}
+
+func BenchmarkWakeup(b *testing.B) {
+	jobN := 100000
+	jobs := newJobs()
+	now := time.Now().Unix()
+	var marchId int64 = 0
+	events := []int{
+		12,  // arrive
+		15,  // gather done
+		20,  // back home
+		501, // speedup
+		502, // recall
+	}
+	for i := 0; i < jobN; i++ {
+		marchId++
+		uid := rand.Int63n(10000) + 1
+		x := rand.Intn(1024)
+		y := rand.Intn(512)
+		event := events[rand.Intn(len(events))]
+		at := time.Now().Add(time.Duration(rand.Intn(1000)) * time.Second)
+		jobs.sched(march{Uid: uid, MarchId: marchId, At: int(at.Unix()), Evt: event, X: x, Y: y})
+	}
+	for i := 0; i < b.N; i++ {
+		jobs.wakeup(now)
+	}
+
+}
+
+func BenchmarkSched(b *testing.B) {
+	var marchId int64 = 0
+	jobs := newJobs()
+	for i := 0; i < b.N; i++ {
+		marchId++
+		uid := rand.Int63n(10000) + 1
+		x := rand.Intn(1024)
+		y := rand.Intn(512)
+		event := 12
+		at := time.Now().Add(time.Duration(rand.Intn(1000)) * time.Second)
+		jobs.sched(march{Uid: uid, MarchId: marchId, At: int(at.Unix()), Evt: event, X: x, Y: y})
+	}
 
 }
