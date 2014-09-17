26d6c73acfa7ae9d7204f3b9cb2d4e1c997952d5
funkygao <funky.gao@gmail.com>
2014-09-17 21:36:28 +0800

    bench comparison of throughput

--- bench/encoding/bench_test.go
+++ bench/encoding/bench_test.go
@@ -11,12 +11,15 @@ func BenchmarkStringsSplit(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		strings.SplitN(s, ",", 1)
 	}
+	b.SetBytes(int64(len(s)))
 
 }
 
 func BenchmarkJsonUnmarshal(b *testing.B) {
+	const s = `{"uid":189845,"march_id":1444}`
 	var v map[string]interface{}
 	for i := 0; i < b.N; i++ {
-		json.Unmarshal([]byte(`{"uid":189845,"march_id":1444}`), v)
+		json.Unmarshal([]byte(s), v)
 	}
+	b.SetBytes(int64(len(s)))
 }
