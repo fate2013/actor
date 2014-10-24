6d0bf5c8cc4af130edf290e03288a3541730b97b
funkygao <funky.gao@gmail.com>
2014-10-24 23:54:36 +0800

    a basic api skeleton

--- actor/api.go
+++ actor/api.go
@@ -1,1 +1,23 @@
 package actor
+
+import (
+	"github.com/funkygao/golib/server"
+	"net/http"
+)
+
+type ApiRunner struct {
+}
+
+func (this *ApiRunner) Run() {
+	server.LaunchHttpServ(":9898", "") // TODO
+	server.RegisterHttpApi("/q/{uid}",
+		func(w http.ResponseWriter, req *http.Request,
+			params map[string]interface{}) (interface{}, error) {
+			return this.handleHttpQuery(w, req, params)
+		}).Methods("GET")
+}
+
+func (this *ApiRunner) handleHttpQuery(w http.ResponseWriter, req *http.Request,
+	params map[string]interface{}) (output interface{}, err error) {
+	return
+}
