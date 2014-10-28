ca4c81d309be13978b6971c2109f4b673bab27fc
funkygao <funky.gao@gmail.com>
2014-10-28 21:43:05 +0800

    lock and unlock

--- actor/api.go
+++ actor/api.go
@@ -14,7 +14,7 @@ type ApiRunner struct {
 
 func (this *ApiRunner) Run() {
 	server.LaunchHttpServ(":9898", "") // TODO
-	server.RegisterHttpApi("/lock/{uid}",
+	server.RegisterHttpApi("/{op}/{uid}",
 		func(w http.ResponseWriter, req *http.Request,
 			params map[string]interface{}) (interface{}, error) {
 			return this.handleHttpQuery(w, req, params)
@@ -25,9 +25,15 @@ func (this *ApiRunner) handleHttpQuery(w http.ResponseWriter, req *http.Request,
 	params map[string]interface{}) (interface{}, error) {
 	var (
 		vars   = mux.Vars(req)
+		op     = vars["op"] // lock | unlock
 		uid    = vars["uid"]
 		output = make(map[string]interface{})
 	)
+
+	switch op {
+	case API_OP_LOCK:
+	case API_OP_UNLOCK:
+	}
 	output["uid"] = uid
 	output["ok"] = 1
 	return output, nil
