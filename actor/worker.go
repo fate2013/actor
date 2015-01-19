296abf0ae013f243465262ae80722b445314324e
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-01-19 10:32:01 +0800

    retrieve worker interface

--- actor/worker.go
+++ actor/worker.go
@@ -1,31 +1,6 @@
-package config
+package actor
 
-import (
-	conf "github.com/funkygao/jsconf"
-)
-
-type ConfigWorker struct {
-	Php ConfigWorkerPhp
-	Pnb ConfigWorkerPnb
-    Rtm ConfigWorkerRtm
-}
-
-func (this *ConfigWorker) loadConfig(cf *conf.Conf) {
-	section, err := cf.Section("php")
-	if err != nil {
-		panic(err)
-	}
-	this.Php.loadConfig(section)
-
-	section, err = cf.Section("pnb")
-	if err != nil {
-		panic(err)
-	}
-	this.Pnb.loadConfig(section)
-
-    section, err = cf.Section("rtm")
-    if err != nil {
-        panic(err)
-    }
-    this.Rtm.loadConfig(section)
+type Worker interface {
+	Start()
+	Wake(w Wakeable)
 }
